3cbf30a7e0a858273ea265081b9d7d701bcc8aac
Gokul Vijayan <gokulv388905@gmail.com>
2026-06-29 07:19:29 +0000

    Modified and optimized the queries

--- models/bronze/stg_customers.sql
+++ models/bronze/stg_customers.sql
@@ -1,11 +1,4 @@
 {{ config(materialized='view') }}
 
-select
-    customer_id,
-    customer_name,
-    email,
-    city,
-    state,
-    country,
-    signup_date
-from {{ source('retail_raw', 'CUSTOMERS_RAW') }};
+select *
+from {{ source('retail_raw', 'CUSTOMERS_RAW') }}
