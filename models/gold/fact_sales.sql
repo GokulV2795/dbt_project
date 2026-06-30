e71c4cb6b72c3a3379c6be869857b784a9c6ea85
Gokul Vijayan <gokulv388905@gmail.com>
2026-06-30 03:19:16 +0000

    Added respective columns in Fact

--- models/gold/fact_sales.sql
+++ models/gold/fact_sales.sql
@@ -1,12 +1,26 @@
-{{ config(
-    materialized='incremental',
-    unique_key='order_item_id'
-) }}
+{{ config(materialized='table') }}
 
-select *
-
+select
+    order_item_id,
+    order_id,
+    order_date,
+    customer_id,
+    customer_name,
+    product_id,
+    product_name,
+    category,
+    brand,
+    store_id,
+    store_name,
+    quantity,
+    unit_price,
+    discount_percent,
+    gross_sales,
+    discount_amount,
+    net_sales,
+    total_cost,
+    profit
 from {{ ref('int_sales') }}
-
 {% if is_incremental() %}
 
 where order_date >
